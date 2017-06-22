8234727c39b190c61d692eb6fab33ae43c77c626
Bruno Pettorelli <bpettorelli@gmail.com>
2017-06-22 19:41:10 +0200

    local mysql port

--- dtn/interface/includes/connect.inc.php
+++ dtn/interface/includes/connect.inc.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new PDO('mysql:host=127.0.0.1;port=3306;charset=utf8;dbname=dtn_htfff', "htfff", "ht!fff_2k15");
+	$conn = new PDO('mysql:host=127.0.0.1;port=3307;charset=utf8;dbname=dtn_htfff', "htfff", "ht!fff_2k15");
 
 function deconnect()
 {
