47c58dc913bf2f7065823e480d2fc4b83ba1dfea
Bruno Pettorelli <bpettorelli@gmail.com>
2018-01-07 11:54:37 +0100

    add style.css to file

--- dtn/interface/joueurs/formAddHistoJoueur.php
+++ dtn/interface/joueurs/formAddHistoJoueur.php
@@ -4,6 +4,7 @@
 <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
 
 <link rel="stylesheet" type="text/css" href="../css/popup.css" />
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
 
 </head>
 <body>
