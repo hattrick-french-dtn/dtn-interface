3edf44081eea7ebaebf8b6da657bb470e0b93981
alpa <robbiecousto@gmail.com>
2019-07-07 08:31:32 +0200

    modif mineure version allemande page DTN

--- members_de.php
+++ members_de.php
@@ -8,7 +8,67 @@ Diese Hintergrundarbeiter haben die Aufgabe die franz&ouml;sichen Spieler mit de
 <strong>Diese &ouml;ffentliche Bekanntgebung folgt einer Erwartung vieler ausl&auml;ndischen Managern</strong>, die sicher gehen wollen, 
 dass die Ansprechpartner auch wirklich der Gruppe angeh&ouml;ren. Die Entscheidung war leicht, zumal wir einige ausl&auml;ndische Benutzer haben.<br/><br/>
 
-<strong>Wir nutzen die Gelegenheit und danken diesen Personen, die Zeit für HT-Frankreich opfern.</strong>
+<strong>Unten finden Sie die Name der Französische U20- und NT- Scouts.</strong>
+<br/><br/>
+<strong>Vous trouverez ci-dessous les listes des DTN+ pour les &eacute;quipes U-20 et A : </strong>
+			</div>
+		</td>
+	 </tr>
+	 <tr><td>
+	 	<table width="100%" border="0" cellspacing="0" cellpadding="1">
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">U-20|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><strong><span class="style40">goalKeepers|</span></strong></div></td>
+          <td width="18%"><div align="right"><span class="style40">porteros|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">gardiens|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">m&aring;lvakter|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Torwart|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">gK</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=12562654"><strong>HypnooS</strong></a>]</div></td>
+        </tr>
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">U-20|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><strong><span class="style40">defenders|</span></strong></div></td>
+          <td width="18%"><div align="right"><span class="style40">defensors|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">d&eacute;fenseurs|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">backar|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Verteidiger|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">cD+wB</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=4629925">Imperatrice_Tatane</a></strong>]</div></td>
+        </tr>
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">U-20|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><span class="style40"><strong>innerMidfielders</strong>|</span></div></td>
+          <td width="18%"><div align="right"><span class="style40">medios campista|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">milieux|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">mittf&auml;ltare|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Mittelfeldspieler|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">iM</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=12066508">titounesmile</a></strong>]</div></td>
+        </tr>
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">U-20|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><span class="style40"><strong>Wingers</strong>|</span></div></td>
+          <td width="18%"><div align="right"><span class="style40">medios lateral|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">ailiers|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">ytter|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Fl&uuml;gelspieler|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">Wg</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11567498">keseru</a></strong>]</div></td>
+        </tr>
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">U-20|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><span class="style40"><strong>Forwards</strong>|</span></div></td>
+          <td width="18%"><div align="right"><span class="style40">delanteros|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">attaquants|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">anfallare|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">St&uuml;rmer|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">Fw</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=7514635">LA-alpacstily</a></strong>]</div></td>
+        </tr>
+      </table>
+	 </td></tr>
+	 <tr><td>&nbsp;</td></tr>
 <br/><br/>
 			</div>
 		</td>
@@ -16,58 +76,64 @@ dass die Ansprechpartner auch wirklich der Gruppe angeh&ouml;ren. Die Entscheidu
 	 <tr><td>
 	 	<table width="100%" border="0" cellspacing="0" cellpadding="1">
         <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
           <td width="13%"><div align="right"><strong><span class="style40">goalKeepers|</span></strong></div></td>
           <td width="18%"><div align="right"><span class="style40">porteros|</span></div></td>
           <td width="14%"><div align="right"><span class="style40">gardiens|</span></div></td>
           <td width="10%"><div align="right"><span class="style40">m&aring;lvakter|</span></div></td>
-          <td width="16%"><div align="right"><span class="style40">torwart|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Torwart|</span></div></td>
           <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">gK</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<a href="./dtn_members.php?poste=gk"><strong>membres</strong></a>]</div></td>
+          <td width="27%"><div align="left">[<a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13159462"><strong>JackAbbott</strong></a>]</div></td>
         </tr>
         <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
           <td width="13%"><div align="right"><strong><span class="style40">defenders|</span></strong></div></td>
           <td width="18%"><div align="right"><span class="style40">defensors|</span></div></td>
           <td width="14%"><div align="right"><span class="style40">d&eacute;fenseurs|</span></div></td>
           <td width="10%"><div align="right"><span class="style40">backar|</span></div></td>
-	        <td width="16%"><div align="right"><span class="style40">innenverteidiger|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Verteidiger|</span></div></td>
           <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">cD+wB</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<strong><a href="./dtn_members.php?poste=cdwb">membres</a></strong>]</div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11609436">KCelec</a></strong>]</div></td>
         </tr>
         <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
           <td width="13%"><div align="right"><span class="style40"><strong>innerMidfielders</strong>|</span></div></td>
           <td width="18%"><div align="right"><span class="style40">medios campista|</span></div></td>
           <td width="14%"><div align="right"><span class="style40">milieux|</span></div></td>
           <td width="10%"><div align="right"><span class="style40">mittf&auml;ltare|</span></div></td>
-          <td width="16%"><div align="right"><span class="style40">mittelfeldspieler|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Mittelfeldspieler|</span></div></td>
           <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">iM</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<strong><a href="./dtn_members.php?poste=im">membres</a></strong>]</div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=9839467">Eremanth</a></strong>]</div></td>
         </tr>
         <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
           <td width="13%"><div align="right"><span class="style40"><strong>Wingers</strong>|</span></div></td>
           <td width="18%"><div align="right"><span class="style40">medios lateral|</span></div></td>
           <td width="14%"><div align="right"><span class="style40">ailiers|</span></div></td>
           <td width="10%"><div align="right"><span class="style40">ytter|</span></div></td>
-	        <td width="16%"><div align="right"><span class="style40">fl&uuml;gelspieler|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Fl&uuml;gelspieler|</span></div></td>
           <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">Wg</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<strong><a href="./dtn_members.php?poste=wg">membres</a></strong>]</div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=5372875">Polatch</a></strong>]</div></td>
         </tr>
         <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
           <td width="13%"><div align="right"><span class="style40"><strong>Forwards</strong>|</span></div></td>
           <td width="18%"><div align="right"><span class="style40">delanteros|</span></div></td>
           <td width="14%"><div align="right"><span class="style40">attaquants|</span></div></td>
           <td width="10%"><div align="right"><span class="style40">anfallare|</span></div></td>
-	        <td width="16%"><div align="right"><span class="style40">st&uuml;rmer|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">St&uuml;rmer|</span></div></td>
           <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">Fw</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<strong><a href="./dtn_members.php?poste=fw">membres</a></strong>]</div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=2777249">flam_</a></strong>]</div></td>
         </tr>
-		<tr>
-          <td width="13%"><div align="right"><strong><span class="style40">others|</span></strong></div></td>
-          <td width="18%"><div align="right"><span class="style40">otros|</span></div></td>
-          <td width="14%"><div align="right"><span class="style40">autres|</span></div></td>
-          <td width="10%"><div align="right"><span class="style40">&ouml;vriga|</span></div></td>
-          <td width="16%"><div align="right"><span class="style40">andere|</span></div></td>
-          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">&nbsp;</span></strong></span></div></td>
-          <td width="27%"><div align="left">[<a href="./dtn_members.php?poste=staff"><strong>staff</strong></a>]</div></td>
+        <tr>
+          <td width="8%"><div align="right"><span class="style40"><strong><span class="style17">A/NT|</span></strong></span></div></td>
+          <td width="13%"><div align="right"><span class="style40"><strong>Long Shots</strong>|</span></div></td>
+          <td width="18%"><div align="right"><span class="style40">Tiros lejanos|</span></div></td>
+          <td width="14%"><div align="right"><span class="style40">Tir de loin|</span></div></td>
+          <td width="10%"><div align="right"><span class="style40">Tiri da fuori|</span></div></td>
+          <td width="16%"><div align="right"><span class="style40">Weitschusse|</span></div></td>
+          <td width="2%"><div align="right"><span class="style40"><strong><span class="style17">TdL</span></strong></span></div></td>
+          <td width="27%"><div align="left">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13418869">abrincate</a></strong>]</div></td>
         </tr>
       </table>
 	 </td></tr>
