2e5897594032c931a19045cfabe957b5a559ced6
_yajog_ <127855350+yajog@users.noreply.github.com>
2023-04-03 23:58:27 +0200

    Mise-à-jour de la liste des DTN+

--- members_fr.php
+++ members_fr.php
@@ -20,74 +20,24 @@ d'autant que nous avions quelques utilisateurs expatri&eacute;s. <br/><br/>
 	 <tr><td>
 	 	<table width="100%" border="0" cellspacing="0" cellpadding="1">
         <tr>
-        <tr>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">gK</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">cD+wB</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">iM</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">Wg</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">Fw</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">16 y/o</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">17 y/o</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">18-21 y/o</span></strong></span></div></td>
         </tr>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-20</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-20</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-20</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-17</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-20</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">U-20</span></strong></span></div></td>
         </tr>
         <tr>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=12562654"><strong>HypnooS</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=4629925">Imperatrice_Tatane</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=9084806">walde-marre</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11567498">keseru</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=7514635">LA-alpacstily</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=3645832"><strong>sebbie</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=3645832">sebbie</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=2663299">Drumtoon</a></strong>]</div></td>
         </tr>
+        <tr>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13076989"><strong>MA-Delnaja</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13076989">MA-Delnaja</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=6142949">KCoach__Alex</a></strong>]</div></td>
         </tr>
-          <td width="8%"><div align="center"><span class="style40">GoalKeepers</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Defenders</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">InnerMidfielders</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Wingers</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Forwards</span></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Porteros</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Defensors</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Medios campista</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Medios lateral</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Delanteros</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Gardiens</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Défenseurs</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Milieux</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Ailiers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Attaquants</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Portieri</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Difensori</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Centrocampisti</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Ali</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Attaccanti</span></strong></span></div></td>
-        </tr>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Torwart</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Verteidiger</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Mittelfeldspieler</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Flügelspieler</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Stürmer</span></strong></span></div></td>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Keepers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Verdedigers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Centrale middenvelders</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Vleugelspelers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Spitsen</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">M&aring;lvakter</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">F&ouml;rsvarare</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Innermittf&auml;ltare</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Yttrar</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Anfallare</span></strong></span></div></td>
         </tr>
       </table>
 	 </td></tr>
@@ -97,86 +47,35 @@ d'autant que nous avions quelques utilisateurs expatri&eacute;s. <br/><br/>
 		</td>
 	 </tr>
 	 <tr><td>
-	 	<table width="100%" border="0" cellspacing="0" cellpadding="1">
+	 	  <table width="100%" border="0" cellspacing="0" cellpadding="1">
         <tr>
-        <tr>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">cD-iM-Wg-Fw</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">cD-iM-Wg-Fw</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">cD-iM-Wg-Fw</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">gK</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">cD+wB</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">iM</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">Wg</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">Fw</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">LS/TdL</span></strong></span></div></td>
         </tr>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
+        <tr>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">22-27 y/o</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">27-28 y/o</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">28+ y/o</span></strong></span></div></td>
+          <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">22+ y/o</span></strong></span></div></td>
+        </tr>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
           <td width="8%"><div align="center"><span class="style40"><strong><span class="style17">NT/A</span></strong></span></div></td>
         </tr>
         <tr>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13159462"><strong>JackAbbott</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11609436">KCelec</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=9839467">Eremanth</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13418869">abrincate</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=2777249">flam_</a></strong>]</div></td>
-          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=13418869">abrincate</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11567498"><strong>Keseru</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=11550808">Poupoul</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=7425121">sessui_du_mil4</a></strong>]</div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=5031559">ptit-philou</a></strong>]</div></td>
         </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">GoalKeepers</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Defenders</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">InnerMidfielders</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Wingers</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Forwards</span></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Long Shots</span></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Porteros</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Defensors</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Medios campista</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Medios lateral</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Delanteros</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Tiros lejanos</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Gardiens</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Défenseurs</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Milieux</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Ailiers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Attaquants</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Tirs lointains</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Portieri</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Difensori</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Centrocampisti</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Ali</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Attaccanti</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Tiri da fuori</span></strong></span></div></td>
-        </tr>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Torwart</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Verteidiger</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Mittelfeldspieler</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Flügelspieler</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Stürmer</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Weitschusse</span></strong></span></div></td>
-        </tr>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">Keepers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Verdedigers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Centrale middenvelders</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Vleugelspelers</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Spitsen</span></strong></span></div></td>
-		  <td width="8%"><div align="center"><span class="style40">Afstandsschoten</span></strong></span></div></td>
-        </tr>
-          <td width="8%"><div align="center"><span class="style40">M&aring;lvakter</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">F&ouml;rsvarare</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Innermittf&auml;ltare</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Yttrar</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">Anfallare</span></strong></span></div></td>
-          <td width="8%"><div align="center"><span class="style40">L&aring;ngskott</span></strong></span></div></td>
+        <tr>
+          <td width="8%"><div align="center"></div></td>
+          <td width="8%"><div align="center"></div></td>
+          <td width="8%"><div align="center">[<strong><a href="https://www.hattrick.org/goto.ashx?path=/Club/Manager/?userId=5081277">Nanus85</a></strong>]</div></td>
+          <td width="8%"><div align="center"></div></td>
         </tr>
       </table>
 	 </td></tr>
